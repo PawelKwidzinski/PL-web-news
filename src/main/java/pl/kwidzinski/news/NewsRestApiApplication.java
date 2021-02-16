2dfaae3d3c629e8d453d3752e53cbabf8d4fd3f2
PawelKwidzinski <solvap83@gmail.com>
2021-02-16 11:18:09 +0100

    Initial commit

---
+++ src/main/java/pl/kwidzinski/news/NewsRestApiApplication.java
@@ -0,0 +1,13 @@
+package pl.kwidzinski.news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kwidzinski/news/NewsRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kwidzinski.news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
