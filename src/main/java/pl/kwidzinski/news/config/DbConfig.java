67417f3800087921f279e5fca0f02b720cc5aae3
PawelKwidzinski <solvap83@gmail.com>
2021-02-22 19:15:34 +0100

    Init DB with new data from News Api: technology_news, science_news, health_news, entertainment_news

--- src/main/java/pl/kwidzinski/news/config/DbConfig.java
+++ src/main/java/pl/kwidzinski/news/config/DbConfig.java
@@ -33,29 +33,49 @@ public class DbConfig {
     public void initDB() {
         final String sqlGeneralDropTable = sqlDropTable(Category.GENERAL.getTableName());
         getJdbcTemplate().update(sqlGeneralDropTable);
-
         final String sqlBusinessDropTable = sqlDropTable(Category.BUSINESS.getTableName());
         getJdbcTemplate().update(sqlBusinessDropTable);
-
         final String sqlSportsDropTable = sqlDropTable(Category.SPORT.getTableName());
         getJdbcTemplate().update(sqlSportsDropTable);
+        final String sqlTechnologyDropTable = sqlDropTable(Category.TECHNOLOGY.getTableName());
+        getJdbcTemplate().update(sqlTechnologyDropTable);
+        final String sqlScienceDropTable = sqlDropTable(Category.SCIENCE.getTableName());
+        getJdbcTemplate().update(sqlScienceDropTable);
+        final String sqlHealthDropTable = sqlDropTable(Category.HEALTH.getTableName());
+        getJdbcTemplate().update(sqlHealthDropTable);
+        final String sqlHealthDropEntertainment = sqlDropTable(Category.ENTERTAINMENT.getTableName());
+        getJdbcTemplate().update(sqlHealthDropEntertainment);
 
         final String sqlCreateTableGeneral = sqlCreateTable(Category.GENERAL.getTableName());
         getJdbcTemplate().update(sqlCreateTableGeneral);
-
         final String sqlCreateTableBusiness = sqlCreateTable(Category.BUSINESS.getTableName());
         getJdbcTemplate().update(sqlCreateTableBusiness);
-
         final String sqlCreateTableSport = sqlCreateTable(Category.SPORT.getTableName());
         getJdbcTemplate().update(sqlCreateTableSport);
+        final String sqlCreateTableTechnology = sqlCreateTable(Category.TECHNOLOGY.getTableName());
+        getJdbcTemplate().update(sqlCreateTableTechnology);
+        final String sqlCreateTableScience = sqlCreateTable(Category.SCIENCE.getTableName());
+        getJdbcTemplate().update(sqlCreateTableScience);
+        final String sqlCreateTableHealth = sqlCreateTable(Category.HEALTH.getTableName());
+        getJdbcTemplate().update(sqlCreateTableHealth);
+        final String sqlCreateTableEntertainment = sqlCreateTable(Category.ENTERTAINMENT.getTableName());
+        getJdbcTemplate().update(sqlCreateTableEntertainment);
 
         final String sqlInsertGeneral = insertData(Category.GENERAL.getTableName());
         final String sqlInsertBusiness = insertData(Category.BUSINESS.getTableName());
         final String sqlInsertSport = insertData(Category.SPORT.getTableName());
+        final String sqlInsertTechnology = insertData(Category.TECHNOLOGY.getTableName());
+        final String sqlInsertScience = insertData(Category.SCIENCE.getTableName());
+        final String sqlInsertHealth = insertData(Category.HEALTH.getTableName());
+        final String sqlInsertEntertainment = insertData(Category.ENTERTAINMENT.getTableName());
 
         insertRestData(sqlInsertGeneral, Country.PL.getName(), Category.GENERAL.name().toLowerCase());
         insertRestData(sqlInsertBusiness, Country.PL.getName(), Category.BUSINESS.name().toLowerCase());
         insertRestData(sqlInsertSport, Country.PL.getName(), Category.SPORT.name().toLowerCase());
+        insertRestData(sqlInsertTechnology, Country.PL.getName(), Category.TECHNOLOGY.name().toLowerCase());
+        insertRestData(sqlInsertScience, Country.PL.getName(), Category.SCIENCE.name().toLowerCase());
+        insertRestData(sqlInsertHealth, Country.PL.getName(), Category.HEALTH.name().toLowerCase());
+        insertRestData(sqlInsertEntertainment, Country.PL.getName(), Category.ENTERTAINMENT.name().toLowerCase());
     }
 
     private String sqlDropTable(String tableName) {
